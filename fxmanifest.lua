4c640fe9e0d6f83c3aabbce0249c4e105207a2c7
TryLooney <luisfelipe.rs08x@gmail.com>
2024-06-22 14:14:30 -0300

    chore: initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'allowlist'
+repository 'https://github.com/TryLooney/allowlist-fivem'
+version '1.0.0'
+
+server_scripts {
+  'server/main.lua',
+}
---
+++ server/main.lua
@@ -0,0 +1,43 @@
+local projectId = ""
+local accessToken = ""
+
+AddEventHandler("playerConnecting", function(name, setKickReason, deferrals)
+  deferrals.defer()
+
+  local source = source
+
+  deferrals.update('Checando allowlist...')
+
+  Wait(0)
+  
+  local steamId
+  local identifiers = GetPlayerIdentifiers(source)
+  deferrals.defer()
+
+  for _, v in pairs(identifiers) do
+      if string.find(v, "steam") then
+          steamId = tonumber(v:sub(7), 16)
+          break
+      end
+  end
+
+  Wait(0)
+
+  if not steamId then
+      deferrals.done("Certifique-se que sua Steam está aberta.")
+      return
+  end
+
+  PerformHttpRequest("https://api.yorpex.app/allowlist/get-by-identifier/" .. tostring(steamId) .. "?projectId="..projectId, function (errorCode, resultData, resultHeaders, errorData)
+    local isApproved = json.decode(resultData)['isApproved'] == true
+
+    if not isApproved then
+      deferrals.done("Você ainda não fez sua allowlist, acesse https://template-01.yorpex.app/allowlist para fazer.")
+    else
+      deferrals.done()
+    end
+  end, 'GET', '', {
+    ['Content-Type'] = 'application/json',
+    ['Authorization'] = 'Bearer ' .. accessToken
+  })
+end)
